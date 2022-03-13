191d02f738fceea117d6378e32ec3a47184bd7cc
sirgama <sirengogamaliel@gmail.com>
2022-03-14 00:08:00 +0300

    Style: add hover effect on portfolio

--- js/script.js
+++ js/script.js
@@ -27,6 +27,62 @@ $(document).ready(function() {
         $(".what3").toggle();
     });
 
+
+    $(".card1").mouseover(function(){
+        $(".card-title1").slideToggle();
+    });
+    $(".card1").mouseleave(function(){
+        $(".card-title1").slideToggle();
+    });
+
+    $(".card2").mouseover(function(){
+        $(".card-title2").slideToggle();
+    });
+    $(".card2").mouseleave(function(){
+        $(".card-title2").slideToggle();
+    });
+
+    $(".card3").mouseover(function(){
+        $(".card-title3").slideToggle();
+    });
+    $(".card3").mouseleave(function(){
+        $(".card-title3").slideToggle();
+    });
+
+    $(".card4").mouseover(function(){
+        $(".card-title4").slideToggle();
+    });
+    $(".card4").mouseleave(function(){
+        $(".card-title4").slideToggle();
+    });
+
+    $(".card5").mouseover(function(){
+        $(".card-title5").slideToggle();
+    });
+    $(".card5").mouseleave(function(){
+        $(".card-title5").slideToggle();
+    });
+
+    $(".card6").mouseover(function(){
+        $(".card-title6").slideToggle();
+    });
+    $(".card6").mouseleave(function(){
+        $(".card-title6").slideToggle();
+    });
+    $(".card7").mouseover(function(){
+        $(".card-title7").slideToggle();
+    });
+    $(".card7").mouseleave(function(){
+        $(".card-title7").slideToggle();
+    });
+    $(".card8").mouseover(function(){
+        $(".card-title8").slideToggle();
+    });
+    $(".card8").mouseleave(function(){
+        $(".card-title8").slideToggle();
+    });
+
+
    
 
   });
