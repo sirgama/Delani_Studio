4d8dca55db00cb10773e643c299c131ede7fe62e
sirgama <sirengogamaliel@gmail.com>
2022-03-13 18:57:28 +0300

    ft: add toggle option in what we do icons

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $(".img1").click(function() {
+      $(".what1").toggle();
+      $(".img1").toggle();
+    });
+    $(".what1").click(function() {
+        $(".img1").toggle();
+        $(".what1").toggle();
+      });
+
+
+      $(".img2").click(function() {
+        $(".what2").toggle();
+        $(".img2").toggle();
+      });
+      $(".what2").click(function() {
+          $(".img2").toggle();
+          $(".what2").toggle();
+        });
+
+        $(".img3").click(function() {
+            $(".what3").toggle();
+            $(".img3").toggle();
+          });
+          $(".what3").click(function() {
+              $(".img3").toggle();
+              $(".what3").toggle();
+            });
+  });
+  
