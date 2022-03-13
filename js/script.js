a24bb8bea83cd3ddec0e8a704418a09053a08572
sirgama <sirengogamaliel@gmail.com>
2022-03-13 19:44:19 +0300

    style: add title on portfolio images

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
     $(".img1").click(function() {
-      $(".what1").toggle();
-      $(".img1").toggle();
+      $(".what1").fadeToggle();
+      $(".img1").slideToggle();
     });
     $(".what1").click(function() {
-        $(".img1").toggle();
-        $(".what1").toggle();
+        $(".img1").fadeToggle();
+        $(".what1").slideToggle();
       });
 
 
       $(".img2").click(function() {
-        $(".what2").toggle();
-        $(".img2").toggle();
+        $(".what2").fadeToggle();
+        $(".img2").slideToggle();
       });
       $(".what2").click(function() {
-          $(".img2").toggle();
-          $(".what2").toggle();
+          $(".img2").fadeToggle();
+          $(".what2").slideToggle();
         });
 
         $(".img3").click(function() {
-            $(".what3").toggle();
-            $(".img3").toggle();
+            $(".what3").fadeToggle();
+            $(".img3").slideToggle();
           });
           $(".what3").click(function() {
-              $(".img3").toggle();
-              $(".what3").toggle();
+              $(".img3").fadeToggle();
+              $(".what3").slideToggle();
             });
   });
   
